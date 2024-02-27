eb231ad77261f12b7a8a373951ce3a3fd57ff9e5
flooish <110519388+flooish@users.noreply.github.com>
2024-02-27 20:26:53 +0800

    Update ts.java
    对程序进行进一步的迭代与修改

--- ts.java
+++ ts.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        // Adding more functionality
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
     }
 }
+
